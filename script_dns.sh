2ca651442630519fc3caddc0dbebd54cc66477b2
root <root@DebianProjet.localdebian.fr>
2016-12-07 15:05:12 +0000

    Script avec rajout des zones inversées.

--- script_dns.sh
+++ script_dns.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
 
-if [ !-a /etc/bind/db.$1.fr ]; then
+if [ ! -a /etc/bind/db.$1.fr ]; then
 
-cat << EndOfFile > /etc/bind/db.$1.fr
+cat << NFFS > /etc/bind/db.$1.fr
 
-$TTL	86400
-@	IN	SOA	ProjectScripting.$1.fr root.$1.fr. (
+\$TTL	604800
+@	IN	SOA	PS.$1.fr. root.$1.fr. (
 			1
 			604800
 			86400
 			2419200
-			76400 )
-@	IN	NS	ProjectScripting.$1.fr.
-ProjectScripting	IN	A	10.0.2.15
+			604800 )
+@	IN	NS	PS.$1.fr.
 
-EndOfFile
+PS	IN	A	10.0.2.15
 
-cat << EndOfSecondFile > /etc/bind/named.conf.local
+NFFS
 
-zone "$1.fr"{
+cat << LZF >> /etc/bind/named.conf.local
+
+zone "$1.fr" IN {
 	type master;
 	file "etc/bind/db.$1.fr";
 }
 
-EndOfSecondFile
+LZF
 
-bind="service bind9 reload"
+bind="/etc/init.d/bind9 restart"
+
+eval $bind
+
+cat << ILZF >> /etc/bind/named.conf.local
+
+zone "2.0.10.in-addr.arpa" {
+	type master;
+	notify no;
+	file "/etc/bind/db.10";
+}
+
+ILZF
+
+cat << DBIF >> /etc/bind/db.10
+
+\$TTL	604800
+@	IN	SOA	PS.$1.fr. root.$1.fr. (
+			2
+			604800
+			86400
+			2419200
+			604800 )
+@	IN	NS	PS.$1.fr.
+10	IN	PTR	PS.$1.fr.
+
+DBIF
 
 eval $bind
 
 fi
-
-
