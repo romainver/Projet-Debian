630a462bc622136c3b8f3e3c1dd7937a1ba4e928
root <root@DebianProjet.localdebian.fr>
2016-12-08 08:49:16 +0000

    Correction du nom des dommaines + rajout de commentaires

--- script_dns.sh
+++ script_dns.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [ ! -a /etc/bind/db.$1.fr ]; then
+if [ ! -a /etc/bind/db.$1.project.fr ]; then
 
-cat << NFFS > /etc/bind/db.$1.fr
+#Création du fichier de zone
+
+cat << NFFS > /etc/bind/db.$1.project.fr
 
 \$TTL	604800
-@	IN	SOA	PS.$1.fr. root.$1.fr. (
+@	IN	SOA	PS.$1.project.fr. root.$1.project.fr. (
 			1
 			604800
 			86400
 			2419200
 			604800 )
-@	IN	NS	PS.$1.fr.
+@	IN	NS	PS.$1.project.fr.
 
-PS	IN	A	10.0.2.15
+PS	IN	A	10.0.2.15 # Modifier l'ip en fonction de la machine utilisée
 
 NFFS
 
@@ -21,7 +23,7 @@ cat << LZF >> /etc/bind/named.conf.local
 
 zone "$1.fr" IN {
 	type master;
-	file "etc/bind/db.$1.fr";
+	file "etc/bind/db.$1.project.fr";
 };
 
 LZF
@@ -30,9 +32,12 @@ bind="/etc/init.d/bind9 restart"
 
 eval $bind
 
-cat << ILZF >> /etc/bind/named.conf.local
 
-zone "2.0.10.in-addr.arpa" {
+# Création du fichier de zone inversée
+
+cat << ILZF > /etc/bind/named.conf.local
+
+zone "2.0.10.in-addr.arpa" { # Modifier l'ip inversée en fonction de la machine utilisée
 	type master;
 	notify no;
 	file "/etc/bind/db.10";
@@ -43,14 +48,14 @@ ILZF
 cat << DBIF >> /etc/bind/db.10
 
 \$TTL	604800
-@	IN	SOA	PS.$1.fr. root.$1.fr. (
+@	IN	SOA	PS.$1..project.fr. root.$1.project.fr. (
 			2
 			604800
 			86400
 			2419200
 			604800 )
-@	IN	NS	PS.$1.fr.
-10	IN	PTR	PS.$1.fr.
+@	IN	NS	PS.$1.project.fr.
+10	IN	PTR	PS.$1.project.fr.
 
 DBIF
 
